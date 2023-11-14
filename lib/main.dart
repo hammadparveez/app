87b528e6b44850605b446009d7cd54c986e5fe81
Hammad Parveez <h.parveez@bayt.net>
2023-11-15 03:00:56 +0500

    Remove commented line

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
