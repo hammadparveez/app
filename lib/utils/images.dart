346581794f99901146735c8b2b3dd44c9a32174c
Mason Brian <hammadpervez6@gmail.com>
2023-11-25 03:15:17 +0500

    bugfix: fix base image path

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,5 +1,5 @@
 class AppImages {
-  static const _base = 'assets/';
+  static const _base = 'assets';
   static const profileImage = '$_base/images/profile.png';
   static const aboutMe = '$_base/images/about_me.png';
   static const wave = '$_base/images/wave.png';
