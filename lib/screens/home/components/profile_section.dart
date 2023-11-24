c7bc44926d14b67ef99ed8b9912cbd23f5fa1516
Mason Brian <hammadpervez6@gmail.com>
2023-11-25 03:14:36 +0500

    Enhance profile section, seperate about section

--- lib/screens/home/components/profile_section.dart
+++ lib/screens/home/components/profile_section.dart
@@ -21,14 +21,16 @@ class ProfileSection extends StatelessWidget {
         Text("I'm Hammad", style: AppTextStyles.heading3),
         Padding(
           padding: const EdgeInsets.only(top: 8, bottom: 8),
-          child: Text.rich(TextSpan(
-            style: AppTextStyles.heading3,
-            children: [
-              const TextSpan(text: 'F'),
-              WidgetSpan(child: Image.asset(AppImages.appIcon, height: 30)),
-              const TextSpan(text: 'lutter Developer')
-            ],
-          )),
+          child: FittedBox(
+            child: Text.rich(TextSpan(
+              style: AppTextStyles.heading3,
+              children: [
+                const TextSpan(text: 'F'),
+                WidgetSpan(child: Image.asset(AppImages.appIcon, height: 30)),
+                const TextSpan(text: 'lutter Developer')
+              ],
+            )),
+          ),
         ),
         Image.asset(AppImages.wave, width: 180),
         Text('#Android', style: AppTextStyles.heading2),
@@ -45,31 +47,6 @@ class ProfileSection extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(top: 8, bottom: 8),
           child: Text('Development', style: AppTextStyles.heading3),
-        ),
-        const SizedBox(height: 24),
-        FractionallySizedBox(
-          widthFactor: .7,
-          child: Row(
-            children: [
-              Expanded(child: Container(height: 1, color: AppColors.heading1)),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Text(
-                  'About Me',
-                  style: AppTextStyles.heading2,
-                ),
-              ),
-              Expanded(child: Container(height: 2, color: AppColors.heading1)),
-            ],
-          ),
-        ),
-        const SizedBox(height: 12),
-        Container(
-          height: 300,
-          width: 300,
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(image: AssetImage(AppImages.aboutMe))),
         ),
       ],
     );
